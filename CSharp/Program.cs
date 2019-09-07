48a534c69b622131b1273d43ca9db0128ae6f515
vkradio <37143087+vkradio@users.noreply.github.com>
2019-09-07 14:17:07 +0500

    Chapter 1 - F# part

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -9,11 +9,11 @@ namespace CSharp
             var distance = new ManhattanDistance();
             var classifier = new BasicClassifier(distance);
 
-            var trainingPath = @"C:\_svn_wc\xsb\_edu\FSharp\ML for .NET\DigitsRecognizer\Data\trainingsample.csv";
+            var trainingPath = @"C:\_git\DigitsRecognizer\Data\trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
             classifier.Train(training);
 
-            var validationPath = @"C:\_svn_wc\xsb\_edu\FSharp\ML for .NET\DigitsRecognizer\Data\validationsample.csv";
+            var validationPath = @"C:\_git\DigitsRecognizer\Data\validationsample.csv";
             var validation = DataReader.ReadObservations(validationPath);
 
             var correct = Evaluator.Correct(validation, classifier);
---
+++ FSharp/Script.fsx
@@ -0,0 +1,51 @@
+﻿open System.IO
+
+type Observation = { Label: string; Pixels: int[] }
+type Distance = int[] * int[] -> int
+
+let toObservation (csvData:string) =
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..]
+    |> Array.map toObservation
+
+let trainingPath = @"C:\_git\DigitsRecognizer\Data\trainingsample.csv";
+let trainingData = reader trainingPath
+
+let manhattanDistance (pixels1, pixels2) =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun (x, y) -> abs(x - y))
+    |> Array.sum
+
+let euclideanDistance (pixels1, pixels2) =
+    Array.zip pixels1 pixels2
+    |> Array.map (fun (x, y) -> pown (x - y) 2)
+    |> Array.sum
+
+let train (trainingset:Observation[]) (dist:Distance) =
+    let classify (pixels:int[]) =
+        trainingset
+        |> Array.minBy (fun x -> dist (x.Pixels, pixels))
+        |> fun x -> x.Label
+    classify
+
+let manhattanClassifier = train trainingData manhattanDistance
+let euclideanClassifier = train trainingData euclideanDistance
+
+let validationPath = @"C:\_git\DigitsRecognizer\Data\validationsample.csv";
+let validationData = reader validationPath
+
+let evaluate validationSet classifier =
+    validationSet
+    |> Array.averageBy (fun x -> if classifier x.Pixels = x.Label then 1. else 0.)
+    |> printfn "Correct %.3f"
+
+printfn "Manhattan"
+evaluate validationData manhattanClassifier
+printfn "Euclidean"
+evaluate validationData euclideanClassifier
